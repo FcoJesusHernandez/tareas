84b09a46fb6a2143ffe875c059c53d7ed2b30065
Francisco <frjesus1997@gmail.com>
2020-02-21 10:23:19 -0600

    Creacion de Controllador y @index

--- resources/views/tareas/tareasForm.blade.php
+++ resources/views/tareas/tareasForm.blade.php
@@ -5,10 +5,10 @@
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
-                        <div class="card-header">Tareas</div>
+                        <div class="card-header">Crear Tareas</div>
 
                         <div class="card-body">
-                        <a href="{{ action('TareaController@create') }}" class="btn btn-danger btn-sm">Nueva tarea</a>
+
                         </div>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('tareas/{nombre?}', function($nombre="usuario"){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/tarea', 'TareaController');
